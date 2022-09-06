3e59d120cfb02d863519dde39fa18007277df703
Joel Turner <joel_turner@wiwd.uscourts.gov>
2022-09-06 11:47:39 -0500

    Updated reconciliation routine

--- SCCM/services/initiate_global_db_session.py
+++ SCCM/services/initiate_global_db_session.py
@@ -1,7 +1,7 @@
 from SCCM.services.db_session import DbSession
 from SCCM.config.config_model import PLRASettings
 
-config_file = '../config/dev.env'
+config_file = 'SCCM/config/dev.env'
 settings = PLRASettings(_env_file=config_file, _env_file_encoding='utf-8')
 
 db_path = f'{settings.db_base_directory}{settings.db_file}'

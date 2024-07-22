cdcc07b3b7f15363126019f31a8c667021b9903f
JoelTurner <joel_turner@wiwd.uscourts.gov>
2024-07-22 09:11:40 -0500

    fix: prompt for legal name

--- SCCM/bin/state_check_convert.py
+++ SCCM/bin/state_check_convert.py
@@ -244,7 +244,7 @@ def main():
                 session.rollback()
                 raise
 
-        # except:
+        # xcept:
         #     session.rollback()
         #     raise
         # else:
